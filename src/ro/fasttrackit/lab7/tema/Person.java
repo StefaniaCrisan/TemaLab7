bd4a564f0ba0265e5eb0215ea0b0b648bd551458
StefaniaCrisan <stefania.vlaic@yahoo.es>
2022-04-04 19:47:42 +0300

    Initial commit

---
+++ src/ro/fasttrackit/lab7/tema/Person.java
@@ -0,0 +1,19 @@
+package ro.fasttrackit.lab7.tema;
+
+public class Person {
+    private String name;
+    private int age;
+    private boolean married;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    boolean isMarried() {
+        return married;
+    }
+}
