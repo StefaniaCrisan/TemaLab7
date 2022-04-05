f96f2bf2a1e9f6531abe44c9d8f0ba6f41b85565
StefaniaCrisan <stefania.vlaic@yahoo.es>
2022-04-05 15:21:07 +0300

    Exercitiile 3 si 4 cu produs

---
+++ src/ro/fasttrackit/lab7/tema/ExercitiiProduct.java
@@ -0,0 +1,22 @@
+package ro.fasttrackit.lab7.tema;
+
+public class ExercitiiProduct {
+    public static void main(String[] args) {
+        Product produs1 =new Product("lapte",4.5,200,"lactate");
+        Product produs2 =new Product("oua",1,300,"oua");
+        Product produs3 =new Product("faina de grau",2.5,50,"fainoase");
+        printProduse(produs1);
+        printProduse(produs2);
+        printProduse(produs3);
+        System.out.println(produs1.isCategory("lactate"));
+        System.out.println(produs1.isCategory("fainoase"));
+        System.out.println(produs2.hasStock());
+
+    }
+    public static void printProduse(Product produs){
+        System.out.println("Produsul este: "+produs.getName());
+        System.out.println("Pretul acestui produs: "+produs.getPrice());
+        System.out.println("Cantitatea disponibila: "+produs.getQuantity());
+        System.out.println("Se gaseste la categoria: "+produs.getCategory());
+    }
+}
---
+++ src/ro/fasttrackit/lab7/tema/Product.java
@@ -0,0 +1,47 @@
+package ro.fasttrackit.lab7.tema;
+
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    private String category;
+
+    Product(String name, double price, int quantity, String category) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    boolean hasStock() {
+        if (quantity != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isCategory(String category) {
+        if(this.category == category) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
